e397e4bee91328b41888ea33727f05a46081bd9a
Jason Smith <jhs@couch.io>
2010-05-09 03:23:21 +0700

    Show servers in the front page

--- hosting/js/home.js
+++ hosting/js/home.js
@@ -1,1 +1,18 @@
-alert('hi');
+$(document).ready(function() {
+  var error = function(resp) {
+    $('#flash').text(resp.reason
+      ? 'Error: ' + resp.reason
+      : "Something went wrong. Please tell Jason if it is a big deal.").addClass('flash');
+  };
+
+  var hosting = $.couch.db('db');
+  //hosting.allDocs({startkey:'Server/', endkey:'Server0', include_docs:true,
+  $.ajax({url:hosting.uri + '_design/hosting/_list/servers/servers_by_account', error:error,
+    success: function(res) {
+      var ul = $('ul#servers');//.html('');
+      res.forEach(function(row) {
+        ul.append($('<li>' + row.html + '</li>'));
+      });
+    }
+  });
+});
