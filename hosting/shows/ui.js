53b5d1071bc30232041ba4d138561bf59f1613db
Jason Smith <jhs@couch.io>
2010-05-08 23:56:35 +0700

    Automatically pull in Javascript code if it`s there

--- hosting/shows/ui.js
+++ hosting/shows/ui.js
@@ -6,12 +6,13 @@ function(doc, req) {
   var context = {main: doc || {}};
   context.page = req.id || 'home';
   context.title = context.page.charAt(0).toUpperCase() + context.page.slice(1);
-  partials.main = ddoc.templates[context.page];
+  context.js = ddoc.js[context.page];
 
   // XXX: I thought partials inherit the context of their parents?
   var real_context = JSON.parse(JSON.stringify(context));
   ['hd', 'ft', 'sidebar'].forEach(function(x) { context[x] = real_context; });
 
+  partials.main = ddoc.templates[context.page];
   return mustache.to_html(ddoc.templates.interface, context, partials);
   return {headers: {'content-type': 'application/json'}, body: JSON.stringify({ doc:doc, req:req })}; // For debugging
 };
