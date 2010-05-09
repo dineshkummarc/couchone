a756f7671ce9f4d61eb7d347eb5d55d752d7487a
Jason Smith <jhs@couch.io>
2010-05-09 21:27:03 +0700

    Display servers in the main page

--- hosting/js/home.js
+++ hosting/js/home.js
@@ -6,13 +6,34 @@ $(document).ready(function() {
   };
 
   var hosting = $.couch.db('db');
-  //hosting.allDocs({startkey:'Server/', endkey:'Server0', include_docs:true,
-  $.ajax({url:hosting.uri + '_design/hosting/_list/servers/servers_by_account', error:error,
-    success: function(res) {
-      var ul = $('ul#servers');//.html('');
-      res.forEach(function(row) {
-        ul.append($('<li>' + row.html + '</li>'));
-      });
+  hosting.view('hosting/servers_by_account', {error:error,
+    success: function(view) {
+      // Add a pretty date formatter to the scope.
+      view._stamp = function(text, render) {
+         var mil_re = /(\.\d\d\d)$/;
+         var stamp = new Date(this.updated_at.replace(mil_re, ' +0000').replace(/-/g, '/'));
+         return stamp.toGMTString().replace(/ GMT$/, this.updated_at.match(mil_re)[1]); // Put the milliseconds back
+      };
+
+      var cls = 'even';
+      view._class = function(text, render) {
+        cls = (cls == 'even') ? 'odd' : 'even';
+        return cls;
+      };
+
+      $('table#servers tbody').html($($.mustache([
+        '{{#rows}}',
+          '<tr class="{{_class}}">',
+            '<td>{{key}}</td>',
+            '{{#value}}',
+              '<td>{{state}}</td>',
+              '<td>{{_stamp}}</td>',
+              '<td>{{pid}}</td>',
+              '<td>{{port}}</td>',
+            '{{/value}}',
+          '<tr>',
+        '{{/rows}}',
+      ].join('\n'), view)));
     }
   });
 });
