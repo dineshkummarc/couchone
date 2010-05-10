ffe8b8391bb2397a3734d305909e45bf84aae2ba
Jason Smith <jhs@couch.io>
2010-05-10 22:30:13 +0700

    Updates to the inviter maker

--- invites/node/makeinvs.js
+++ invites/node/makeinvs.js
@@ -1,5 +1,8 @@
 // this is a simple importer for the existing set of invites
 
+var host = 'localhost:5985';
+var DB = 'http://coadmin:n60Tj)59.89[@'+host+'/invites';
+
 var csv = require('./lib/csv'),
     request = require("./lib/request"),
     sys = require('sys'),
@@ -22,24 +25,44 @@ fs.readFile(path.join(__dirname, "invite-codes.txt"), function(err, stuff) {
   codes = codes.sort(function() {return Math.random() - 0.5});
   
   function newInvite(doc) {
-    request.request("http://localhost:5984/invites/"+codes.pop(), 'PUT', JSON.stringify(doc),
+    request.request(DB+"/"+codes.pop(), 'PUT', JSON.stringify(doc),
       null, null, null, function(er, response) {
-        // warning, this code never runs due to node bug
-        sys.puts(JSON.stringify(arguments));
-        // todo retry save in case of error
+        if(er) throw new Error(er);
+        sys.puts(JSON.stringify(doc));
       });
   };
   
-  csv.each(path.join(__dirname, "roundone.csv"), {sep:','}).addListener("data", function(data) {
+  /* buggy
+  if(process.env.dry)
+    sys.puts('{docs: [');
+  */
+
+  var lines = csv.each(path.join(__dirname, "roundtwo.csv"));
+  lines.addListener("data", function(data) {
     if (data[0].indexOf("@") != -1) {
      var doc = {
+       type: "invite",
        invite_email : data[0],
+       inviter: data[1],
+       email_sent: true,
        state : "open"
      };
-     newInvite(doc);
+
+     if(process.env.dry) {
+       doc._id = codes.pop();
+       sys.puts('  ' + JSON.stringify(doc) + ',');
+     } else {
+       newInvite(doc);
+     }
     }
   });
-  
-  
+
+  /* buggy
+  lines.addListener('end', function() {
+    sys.puts('  {}');
+    sys.puts(']}');
+  });
+  */
+
 });
 
