1e59761c10f438ecb39c8819bbe0a17d73708581
Jason Smith <jhs@couch.io>
2010-05-12 20:29:26 +0700

    Convert lib into an object

--- hosting/_attachments/js/lib.js
+++ hosting/_attachments/js/lib.js
@@ -1,35 +1,32 @@
-var lib = {
+var lib = new (function() {
 
-  flash_error: function(resp) {
+  this.flash_error = function(resp) {
     $('#flash').text(resp.reason
       ? 'Error: ' + resp.reason
       : "Something went wrong. Please tell Jason if it is a big deal.").addClass('flash');
-  },
+  };
 
-  flash: function(message) {
+  this.flash = function(message) {
     $('#flash').text(message).addClass('flash_message');
-  },
+  };
 
   // Convert the timestamp format used in the DB to a Javascript date.
-  format_ts: function(stamp) {
+  this.format_ts = function(stamp) {
     var mil_re = /(\.\d\d\d)$/;
     var dat = new Date(stamp.replace(mil_re, ' +0000').replace(/-/g, '/'));
     return dat.toGMTString().replace(/ GMT$/, stamp.match(mil_re)[1]); // Put the milliseconds back.
-  },
+  };
 
   // Alternate between returning "odd" and "even".
-  odd_even: function() {
-    var val = 'even';
-    return function() {
-      val = (val == 'even') ? 'odd' : 'even';
-      return val;
-    };
-  }(),
+  var val = 'even';
+  this.odd_even = function() {
+    val = (val == 'even') ? 'odd' : 'even';
+    return val;
+  };
 
-  mustache: function(context) {
+  this.mustache = function(context) {
     var lines = Array.prototype.slice.apply(arguments, [1]);
     return $($.mustache(lines.join("\n"), context));
-  },
+  };
 
-  _: undefined
-};
+})();
