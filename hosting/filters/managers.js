ad41e48b75a60d8f1abe80d48021cf7ed505e672
Jason Smith <jhs@couch.io>
2010-07-10 18:03:20 +0700

    Allow filtering for a particular manager

--- hosting/filters/managers.js
+++ hosting/filters/managers.js
@@ -1,4 +1,4 @@
 function(doc, req) {
   var vf = require('js/lib/vf');
-  return vf.is_manager(doc);
+  return vf.is_manager(doc, req.query.id);
 }
--- hosting/js/lib/vf.js
+++ hosting/js/lib/vf.js
@@ -2,6 +2,10 @@
 // Common functionality for views and filters
 //
 
-exports.is_manager = function(doc) {
-  return doc._id && doc._id.indexOf('Manager/') == 0;
+// Return whether a document is a hosting manager, optionally if it is a particular one.
+exports.is_manager = function(doc, id) {
+  if(!doc._id)
+    return false;
+  var match = doc._id.match(/^Manager\/(\w+)$/);
+  return match && (id === undefined || match[1] == id);
 }
