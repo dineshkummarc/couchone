27d005cafc7dfbe0bb9f66ca41a850c09ff0f011
Jan Lehnardt <jan@apache.org>
2010-07-19 12:43:06 +0200

    Update bounce.html styles to match the rest of the site.
    Add a bit of ajax to deal with domain-duplication:
    
     - on success, the regular procedure is followed,
     - on error, the user gets redirected to
         http://www.couch.io/get?params...
       which displays an error message.
    
    Depends on:
    
     - http://hosting.couch.io/_utils/script/jquery.js
     - http://hosting.couch.io/_utils/script/jquery.couch.js
    
    being available.

--- hosting/updates/docs.js
+++ hosting/updates/docs.js
@@ -1,47 +1,18 @@
 function(doc, req) {
   var ddoc = this;
+  var args = {};
+  req.body.split('&').forEach(function(pair) {
+    pair = pair.split('=');
+    args[pair[0]] = pair[1];
+  });
+  var mustache = require('vendor/mustache/mustache');
 
-  if (req.method == "GET") {
-    return [null, {
-      headers : {
-        "content-type" : "application/json"
-      },
-      body : JSON.stringify(doc)
-    }];
-  } else if (req.method == "PUT") {
-    var newDoc = JSON.parse(req.body);
-    return [newDoc, {json : {id : newDoc._id}}];
-  } else if (req.method == "POST") {
-    log(req);
-    var newDoc;
+  var view = {};
+  view.raw = req.body;
+  view.subdomain = args.subdomain;
+  view.args = args.toSource();
 
-    if(req.headers['Content-Type'] == 'application/json') {
-      newDoc = JSON.parse(req.body);
-    } else {
-      var args = {};
-      req.body.split('&').forEach(function(pair) {
-        pair = pair.split('=');
-        args[pair[0]] = pair[1];
-      })
-      newDoc = {"_id": "Server/"+args.subdomain, "creation": args};
-      var url = "http://" + args.subdomain + '.couchone.com/_utils/';
-
-      if(args.test) {
-        log("Testing only. Ignoring: " + JSON.stringify(newDoc));
-        newDoc = null;
-      }
-
-      var mustache = require('vendor/mustache/mustache');
-      var values = {};
-      values.subdomain = args.subdomain;
-
-      return [newDoc,
-              {"code": 302,
-               "headers": {"Location": url,
-                           "Content-Type": "text/html"
-                          },
-               "body": mustache.to_html(ddoc.templates.bounce, values)
-              }];
-    }
-  }
+  return [null, {
+           "body": mustache.to_html(ddoc.templates.bounce, view)
+          }];
 };
